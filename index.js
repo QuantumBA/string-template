73638a4565be2251f105033ae4ebefa912cabd54
ainsausti <ainsausti@quantum-ba.com>
2018-06-12 12:45:08 +0200

    let the user override the template pattern

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ module.exports = template
 function template(string) {
     var args
 
-    if (arguments.length === 2 && typeof arguments[1] === "object") {
+    if (arguments.length === 3 && typeof arguments[1] === "object") {
+        args = arguments[1]
+        nargs = arguments[2]
+    } else if (arguments.length === 2 && typeof arguments[1] === "object") {
         args = arguments[1]
     } else {
         args = slice.call(arguments, 1)
