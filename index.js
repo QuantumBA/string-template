9e84f555c59da9f107d8cfe6c6bb2f9a28a0c5c4
ainsausti <ainsausti@quantum-ba.com>
2018-06-12 13:10:23 +0200

    Configurable start and end scaping

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var nargs = /\{([0-9a-zA-Z\.]+)\}/g
 var slice = Array.prototype.slice
 var getProp = require('./getProp')
+var scapeInit = "{"
+var scapeEnd = "}"
 
 module.exports = template
 
 function template(string) {
     var args
 
-    if (arguments.length === 3 && typeof arguments[1] === "object") {
+    if (arguments.length === 5 && typeof arguments[1] === "object") {
+        args = arguments[1]
+        nargs = arguments[2]
+        scapeInit = arguments[3]
+        scapeEnd = arguments[4]
+    }
+    else if (arguments.length === 4 && typeof arguments[1] === "object") {
+        args = arguments[1]
+        nargs = arguments[2]
+        scapeInit = arguments[3]
+    }
+    else if (arguments.length === 3 && typeof arguments[1] === "object") {
         args = arguments[1]
         nargs = arguments[2]
     } else if (arguments.length === 2 && typeof arguments[1] === "object") {
@@ -22,8 +35,8 @@ function template(string) {
 
     return string.replace(nargs, function replaceArg(match, i, index) {
         // Escaped brackets
-        if (string[index - 1] === "{" &&
-            string[index + match.length] === "}") {
+        if (string[index - 1] === scapeInit &&
+            string[index + match.length] === scapeEnd) {
             return i
         }
 
